ad19799c1f2dcd9fb15208135831a697ec82d626
kvnbanunu <kvnbanunu@gmail.com>
2026-01-14 16:19:58 -0800

    alacritty/nvim: match color theme

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -5,6 +5,11 @@ return {
 		priority = 1000,
 		config = function()
 			vim.cmd("colorscheme tokyonight")
+			-- vim.cmd("hi Directory guibg=NONE")
+			-- vim.cmd("hi SignColumn guibg=NONE")
+			-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+			-- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
 		end,
 	},
 }
